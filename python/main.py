a1553dada5eafe7f9804d2eb57f7b9c8787e2d86
Micael Lima <micaelcomp@alu.ufc.br>
2023-07-13 18:18:07 -0300

    melhora script python

--- python/main.py
+++ python/main.py
@@ -4,13 +4,11 @@ import time
 import signal
 import datetime 
 
-GCC_COMPILE_C_FILES = "g++ -c Ball.cpp  Cue.cpp main.cpp  Audio.cpp Game.cpp Collision.cpp Point.cpp Wall.cpp Random.cpp Light.cpp Geometry.cpp Floor.cpp  Texture.cpp Model.cpp"
-GCC_COMPILE_MAIN = "g++ Ball.o  Cue.o main.o Point.o Audio.o Game.o Light.o Wall.o Texture.o Model.o Random.o Geometry.o Floor.o Collision.o -o main -lGL -lglut -lassimp -lGLU -lSOIL -lSDL2 -lSDL2_mixer"
+GCC_COMPILE_C_FILES = "g++ -c ../src/*.cpp && g++ *.o -o main -lGL -lglut -lassimp -lGLU -lSOIL -lSDL2 -lSDL2_mixer"
 
 path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(path + '/../src')
 
- 
 def hasFileChanged():
     for filename in os.listdir('../src'):
         if filename.endswith(".cpp") or filename.endswith(".h"):
@@ -20,7 +18,7 @@ def hasFileChanged():
             time_diff = current_time - modify_time
             if time_diff <= 0.20:
                 return True
-    
+
     for filename in os.listdir('./../include'):
         if filename.endswith(".cpp") or filename.endswith(".h"):
             filepath = os.path.join('./../include', filename)
@@ -32,23 +30,18 @@ def hasFileChanged():
     return False
 
 
-def recompile():
-    command = GCC_COMPILE_C_FILES.split()
-    command2 = GCC_COMPILE_MAIN.split()
-
-    subprocess.call(command)
-    subprocess.call(command2)
-
+def recompile():    
+    subprocess.call(GCC_COMPILE_C_FILES,shell = True)
 
 recompile()
 
-process = subprocess.Popen(["./main"])
+process = subprocess.Popen(["./main"], preexec_fn=os.setsid)
 
 while True:
     if hasFileChanged():
-        os.kill(process.pid, signal.SIGINT)
+        os.killpg(os.getpgid(process.pid), signal.SIGINT)
 
         recompile()
-        process = subprocess.Popen(["./main"])
+        process = subprocess.Popen(["./main"], preexec_fn=os.setsid)
 
         time.sleep(1)
